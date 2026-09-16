77f5d803f906a96349a3dcd386bc55300ebeb52f
apaza3000 <96398557+apaza3000@users.noreply.github.com>
2026-09-16 18:58:47 -0500

    CREANDO CONTROLADOR Y MODELO DE AULA

---
+++ app/Http/Controllers/AulaController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aula;
+use Illuminate\Http\Request;
+
+class AulaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        //progrmarte sdfar
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        //validadr
+        //registrar en bd
+        //redirigir haic auna vista
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aula $aula)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aula $aula)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Aula $aula)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aula $aula)
+    {
+        //
+    }
+}
